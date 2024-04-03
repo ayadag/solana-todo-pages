675f970adea50e6f1ba592e71089f70ff59156a9
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-03 20:22:27 +0300

    JitsiMeetComponent

---
+++ app/components/JitsiMeetComponent.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { JitsiMeeting } from "@jitsi/react-sdk";
+
+
+export default function JitsiMeetComponent() {
+
+    const roomName = "room1";
+    const domain = "meet.jit.si";       
+
+    return (
+
+     <div style={{ height: "100vh", display: "grid", flexDirection: "column" }} >
+              <JitsiMeeting 
+                roomName={roomName}
+                displayName={"ayad"}
+                domain={domain}
+                containerStyles={{ display: "flex", flex: 1 }}
+              />
+       </div>
+    );
+};
+
+//export default JitsiMeetComponent;
--- app/pages/meeting.js
+++ app/pages/meeting.js
@@ -2,15 +2,13 @@ import React from 'react'
 import styles from '../styles/Meeting.module.css'
 //import JitsiMeetComponent from '../components/meet/JitsiMeetComponent'
 //import { JitsiMeeting } from '@jitsi/react-sdk'
+import JitsiMeetComponent from '../components/JitsiMeetComponent'
 
 export default function Meeting(){
 
     return(
         <>
-        <div className={styles.container}>
-         <h1>h1</h1>
-         <h2>h2</h2>
-       </div>
+        <JitsiMeetComponent />
        </>
     )
 }
