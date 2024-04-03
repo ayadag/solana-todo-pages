d15dd076e9d29accbad562fa59e47bb5638c722f
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-03 15:16:22 +0300

    deleate

--- app/pages/meeting.js
+++ app/pages/meeting.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import styles from '../styles/Contact.module.css'
-import JitsiMeetComponent from '../components/meet/JitsiMeetComponent'
+//import JitsiMeetComponent from '../components/meet/JitsiMeetComponent'
 
 export default function Meeting(){
 
     return(
         <div className={styles.container}>
-
-        <JitsiMeetComponent />
-
+            <h1>hello</h1>
         </div>
     )
 }
