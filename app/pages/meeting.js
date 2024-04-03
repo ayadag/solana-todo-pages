7af5ef0dbb8400de582a6028921799233f45892f
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-03 14:18:00 +0300

    jmc

--- app/pages/meeting.js
+++ app/pages/meeting.js
@@ -2,18 +2,8 @@ import React from "react";
 import TodoSection from '../components/meet/JitsiMeetComponent';
 
 export default function Meeting(){
-
-    domain = "meet.jit.si";
-    roomName = "room1";
-
+    
     return(
-        <div style={{ height: "100vh", display: "grid", flexDirection: "column" }} >
-        <JitsiMeeting 
-        roomName={roomName}
-        displayName={"ayad"}
-        domain={domain}
-        containerStyles={{ display: "flex", flex: 1 }}
-        />
-        </div>
+        <JitsiMeetComponent />
     )
 }
