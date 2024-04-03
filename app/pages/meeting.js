5bcff6cdde4cf38377d4db83984b69382b8e7d1d
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-03 20:08:34 +0300

    <>

--- app/pages/meeting.js
+++ app/pages/meeting.js
@@ -6,9 +6,11 @@ import { JitsiMeeting } from '@jitsi/react-sdk'
 export default function Meeting(){
 
     return(
+        <>
         <div className={styles.container}>
          <h1>h1</h1>
          <h2>h2</h2>
        </div>
+       </>
     )
 }
