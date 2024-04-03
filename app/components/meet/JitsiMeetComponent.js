96763a20ea3cc3f00eaddb3ac3bcab50a854fb43
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-03 14:56:26 +0300

    url

--- app/components/meet/JitsiMeetComponent.js
+++ app/components/meet/JitsiMeetComponent.js
@@ -4,8 +4,7 @@ import { JitsiMeeting } from "@jitsi/react-sdk";
 
 const JitsiMeetComponent = () => {
 
-    const roomName = "room1";
-    const domain = "meet.jit.si";
+
 
     return (
         <div style={{ height: "100vh", display: "grid", flexDirection: "column" }} >
