d001e67717283877daad845fde4d3cfa9f8da94c
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-03 20:01:38 +0300

    cont

--- app/pages/meeting.js
+++ app/pages/meeting.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../styles/Contact.module.css'
+import styles from '../styles/Meeting.module.css'
 //import JitsiMeetComponent from '../components/meet/JitsiMeetComponent'
 import { JitsiMeeting } from '@jitsi/react-sdk'
 
@@ -7,14 +7,12 @@ export default function Meeting(){
 
     return(
         <div className={styles.container}>
-        <div style={{ height: "100vh", display: "grid", flexDirection: "column" }} >
         <JitsiMeeting 
           roomName={"room1"}
           displayName={"ayad"}
           domain={"meet.jit.si"}
           containerStyles={{ display: "flex", flex: 1 }}
         />
-      </div>
       </div>
     )
 }
