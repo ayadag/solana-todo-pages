128f20fe1e1143a931afc2b2d05047422bf004b7
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-03 07:38:23 +0300

    jitsi 1.4

--- app/pages/meeting.js
+++ app/pages/meeting.js
@@ -1,26 +1,33 @@
 import React from "react";
+import { JitsiMeeting } from '@jitsi/react-sdk';
 import styles from '../styles/Meeting.module.css';
 
-const domain = 'meet.jit.si';
-const options = {
-roomName: 'room1',
-width: 700,
-height: 700,
-parentNode: document.querySelector('#container'),
-lang: 'de'
-};
-const api = new JitsiMeetExternalAPI(domain, options);
+
 
 export default function Meeting(){
     return(
         <>
-
-        <script src='https://meet.jit.si/external_api.js'></script>
-        {api}
-    
-        <div className={styles.container}>
-
-        </div>
+        <JitsiMeeting
+    domain = { YOUR_DOMAIN }
+    roomName = "PleaseUseAGoodRoomName"
+    configOverwrite = {{
+        startWithAudioMuted: true,
+        disableModeratorIndicator: true,
+        startScreenSharing: true,
+        enableEmailInStats: false
+    }}
+    interfaceConfigOverwrite = {{
+        DISABLE_JOIN_LEAVE_NOTIFICATIONS: true
+    }}
+    userInfo = {{
+        displayName: 'YOUR_USERNAME'
+    }}
+    onApiReady = { (externalApi) => {
+        // here you can attach custom event listeners to the Jitsi Meet External API
+        // you can also store it locally to execute commands
+    } }
+    getIFrameRef = { (iframeRef) => { iframeRef.style.height = '400px'; } }
+/>
         </>
     )
 }
