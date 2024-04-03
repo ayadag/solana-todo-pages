d581ae1b8758b7b7fce77db91cd6e307e44a8409
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-03 20:05:37 +0300

    h12

--- app/pages/meeting.js
+++ app/pages/meeting.js
@@ -7,12 +7,8 @@ export default function Meeting(){
 
     return(
         <div className={styles.container}>
-        <JitsiMeeting 
-          roomName={"room1"}
-          displayName={"ayad"}
-          domain={"meet.jit.si"}
-          containerStyles={{ display: "flex", flex: 1 }}
-        />
+        <h1>h1</h1>
+        <h2>h2</h2>
       </div>
     )
 }
