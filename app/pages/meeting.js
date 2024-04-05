bde68fb6f2c2cc65f4e17e962a2f220e30abed5c
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-05 08:00:07 +0300

    meeting page

--- app/pages/meeting.js
+++ app/pages/meeting.js
@@ -1,19 +1,10 @@
-//import React from 'react'
-import styles from '../styles/Meeting.module.css'
-//import JitsiMeetComponent from '../components/meet/JitsiMeetComponent'
-import { JitsiMeeting } from '@jitsi/react-sdk'
-//import JitsiMeetComponent from '../components/JitsiMeetComponent'
+import React from "react";
+import styles from '../styles/Contact.module.css'
 
 export default function Meeting(){
-
     return(
-        <>
         <div className={styles.container}>
-         <JitsiMeeting
-          roomName = { 'room1996' }
-          getIFrameRef = { node => node.style.height = '800px' }
-         />
+            <h2 className={styles.title}>Meeting page</h2>
         </div>
-       </>
     )
 }
