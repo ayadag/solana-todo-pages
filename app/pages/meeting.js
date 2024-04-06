206f5f8b25e29bfa1b0356eb6e206756a79c8fa1
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-06 12:34:34 +0300

    <></>

--- app/pages/meeting.js
+++ app/pages/meeting.js
@@ -3,9 +3,12 @@ import styles from '../styles/Contact.module.css'
 
 export default function Meeting(){
     return(
+        <>
+        <script src="https://meet.jit.si/external_api.js"></script>
+
         <div className={styles.container}>
-            <script src="https://meet.jit.si/external_api.js"></script>
             <h2 className={styles.title}>Meeting page</h2>
         </div>
+        </>
     )
 }
